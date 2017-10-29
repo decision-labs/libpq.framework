ab533877688deed06a03b089e07cb298ab0cfa15
Kashif Rasul <kashif.rasul@gmail.com>
2017-10-29 19:08:53 +0100

    removed unused file

--- src/include/libpq/.c
+++
@@ -1,8 +0,0 @@
-/* Processed by ecpg (10.0) */
-/* These include files are added by the preprocessor */
-#include <ecpglib.h>
-#include <ecpgerrno.h>
-#include <sqlca.h>
-/* End of automatic include section */
-
-#line 1 "/Users/kashif/mobile/libpq.framework/src/include/libpq/."
